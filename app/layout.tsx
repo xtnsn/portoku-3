c0cac893377ec70564e941de2a69abd342f3354f
xtension <47290097+xtnsn@users.noreply.github.com>
2025-05-17 23:36:50 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "v0 App",
+  title: "Fortuna Web - Bikin Website Muraaaaahhh!!",
   description: "Created with v0",
   generator: "v0.dev",
 }
